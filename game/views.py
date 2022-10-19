497859011de2459075a5467466111ddfca724f43
chtholine <oleg123qwe@gmail.com>
2022-10-19 11:59:55 +0300

    added modules

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from game.forms import UserName, InputWord
 
